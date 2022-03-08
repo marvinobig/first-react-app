388807f2e6a9b0377dd094550e0463595997b48b
marvin obig <obig.marv@gmail.com>
2022-03-08 10:07:33 +0000

    changed import links to resemble directory changes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from "react"
 
-import Header from "./Header";
-import Content from "./Content";
-import Footer from "./Footer";
+import Header from "./components/Header";
+import Content from "./components/Content";
+import Footer from "./components/Footer";
 import './App.css';
 
 export default function App() {
