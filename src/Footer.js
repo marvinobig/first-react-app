2154144a216cfa9a1fd5cd354858f710b64b4dfe
marvin obig <obig.marv@gmail.com>
2022-03-08 10:02:40 +0000

    moved components into new directory

--- src/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-
-export default function Footer() {
-    return (
-        <footer>
-            <p>© 2022 Obigwilo development. All rights reserved.</p>
-        </footer>
-    )
-}
